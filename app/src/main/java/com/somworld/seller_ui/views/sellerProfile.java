a724791b2c64b557ee92c750b5b158a1a955559a
somesh9827 <somesh9827@gmail.com>
2015-01-11 00:47:34 +0530

    profile page details added

--- app/src/main/java/com/somworld/seller_ui/views/sellerProfile.java
+++ app/src/main/java/com/somworld/seller_ui/views/sellerProfile.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
 import com.somworld.seller_ui.R;
 
 public class sellerProfile extends BaseActivity {
@@ -11,7 +13,20 @@ public class sellerProfile extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_seller_profile);
+
+        setValue(R.id.shop_name,R.string.shop_name);
+        setValue(R.id.owner_name,R.string.owner_name);
+        setValue(R.id.working_days,R.string.working_days);
+        setValue(R.id.working_hours,R.string.working_hours);
+        setValue(R.id.shop_phone_number,R.string.shop_phone_number);
+        setValue(R.id.shop_address,R.string.shop_address);
+    }
+
+    private void setValue(int id, int stringId) {
+        TextView textElement = (TextView)findViewById(id);
+        textElement.setText(getString(stringId));
     }
 
     @Override
