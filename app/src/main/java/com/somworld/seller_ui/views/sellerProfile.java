3133226988ad9b75082ecd515dccaf39eb6ca4f4
somesh9827 <somesh9827@gmail.com>
2015-01-11 11:23:02 +0530

    edit button added in profile page

--- app/src/main/java/com/somworld/seller_ui/views/sellerProfile.java
+++ app/src/main/java/com/somworld/seller_ui/views/sellerProfile.java
@@ -1,8 +1,6 @@
 package com.somworld.seller_ui.views;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
