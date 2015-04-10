608fe252ada3fdc425771d25dc02b999551219d5
somesh9827 <somesh9827@gmail.com>
2015-04-10 19:59:56 +0530

    Location class added

--- store/src/main/java/com/somworld/store/net/entity/seller/Location.java
+++ store/src/main/java/com/somworld/store/net/entity/seller/Location.java
@@ -1,8 +1,24 @@
 package com.somworld.store.net.entity.seller;
 
+import com.somworld.vollywraper.net.entity.IEntity;
+
 /**
  * Created by somesh.shrivastava on 10/04/15.
  */
-public class Location {
+public class Location implements IEntity {
+  private String latitiude;
+  private String Longtitude;
 
+  public Location(String latitiude, String longtitude) {
+    this.latitiude = latitiude;
+    Longtitude = longtitude;
+  }
+
+  @Override
+  public String toString() {
+    return "Location{" +
+           "latitiude='" + latitiude + '\'' +
+           ", Longtitude='" + Longtitude + '\'' +
+           '}';
+  }
 }
